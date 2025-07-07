851059b81a681fdaef7706028129af65a198aa46
Prox2025 <massinguecarlosantonio@gmail.com>
2025-07-07 22:01:31 +0200

    Update rodape_animado.py

--- rodape_animado.py
+++ rodape_animado.py
@@ -33,26 +33,22 @@ def create_footer():
     mapping.vector_type = 'POINT'
     mapping.inputs['Rotation'].default_value[0] = 1.5708  # 90° em X
 
-    # Limpar color ramp sem erro
     elements = color_ramp.color_ramp.elements
 
     points = [
-        (0.0, 1.0),   # opacidade 1
+        (0.0, 1.0),
         (0.25, 0.7),
         (0.5, 0.4),
         (0.85, 0.1),
         (1.0, 0.0),
     ]
 
-    # Resetar primeiro elemento (sempre existe)
     elements[0].position = points[0][0]
     elements[0].color = (0, 0, 0, points[0][1])
 
-    # Remove os demais elementos (se houver)
     for i in range(len(elements) - 1, 0, -1):
         elements.remove(elements[i])
 
-    # Adiciona os outros pontos do gradiente
     for pos, alpha in points[1:]:
         elem = elements.new(pos)
         elem.color = (0, 0, 0, alpha)
@@ -68,20 +64,24 @@ def create_footer():
 
     rodape.data.materials.append(mat)
 
-    # Animação
     rodape.location.y = -12
-    rodape.keyframe_insert(data_path="location", frame=1)     # 0%
+    rodape.keyframe_insert(data_path="location", frame=1)
     rodape.location.y = -9.5
-    rodape.keyframe_insert(data_path="location", frame=30)    # 10%
-    rodape.keyframe_insert(data_path="location", frame=150)   # 50%
+    rodape.keyframe_insert(data_path="location", frame=30)
+    rodape.keyframe_insert(data_path="location", frame=150)
     rodape.location.y = -12
-    rodape.keyframe_insert(data_path="location", frame=180)   # 60%
-    rodape.keyframe_insert(data_path="location", frame=300)   # 100%
+    rodape.keyframe_insert(data_path="location", frame=180)
+    rodape.keyframe_insert(data_path="location", frame=300)
 
-    # Interpolação linear para suavizar movimento
     for fcurve in rodape.animation_data.action.fcurves:
         for kp in fcurve.keyframe_points:
             kp.interpolation = 'LINEAR'
+
+def create_camera():
+    bpy.ops.object.camera_add(location=(0, 0, 10))
+    cam = bpy.context.active_object
+    cam.rotation_euler = (1.5708, 0, 0)
+    bpy.context.scene.camera = cam
 
 def configure_render():
     scene = bpy.context.scene
@@ -93,18 +93,18 @@ def configure_render():
 
     scene.render.image_settings.file_format = 'FFMPEG'
     scene.render.ffmpeg.format = 'WEBM'
-    scene.render.ffmpeg.codec = 'WEBM'  # Ajustado para codec válido
+    scene.render.ffmpeg.codec = 'WEBM'
     scene.render.ffmpeg.constant_rate_factor = 'HIGH'
     scene.render.ffmpeg.video_bitrate = 1000
     scene.render.image_settings.color_mode = 'RGBA'
     scene.frame_start = 1
-    scene.frame_end = 300  # 10 segundos a 30fps
+    scene.frame_end = 300
     scene.render.fps = 30
     scene.render.resolution_x = 1280
     scene.render.resolution_y = 720
 
-# Execução principal
 clear_scene()
 create_footer()
+create_camera()
 configure_render()
 bpy.ops.render.render(animation=True)
